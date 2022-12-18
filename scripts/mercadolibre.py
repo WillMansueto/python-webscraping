1b5a2225817e3e3bfc014c4bfcd88be78fbc6ad2
Willians Mansueto <31316180+WillMansueto@users.noreply.github.com>
2022-12-18 09:56:25 -0300

    Update mercadolibre.py
    Now the script will save the results to a .csv file with the name of searched term

--- scripts/mercadolibre.py
+++ scripts/mercadolibre.py
@@ -1,28 +1,30 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 url_base = 'https://lista.mercadolivre.com.br/'
-
 produto_nome = input('Qual produto você deseja? ')
-
 response = requests.get(url_base + produto_nome)
-
 site = BeautifulSoup(response.text, 'html.parser')
-
-produtos = site.findAll('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'})
-
+produtos = site.findAll('li', attrs={'class': 'ui-search-layout__item shops__layout-item'})
+df = pd.DataFrame()
 for produto in produtos:
     titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
     link = produto.find('a', attrs={'class': 'ui-search-link'})
 
     real = produto.find('span', attrs={'class': 'price-tag-fraction'})
+    real = real.text.replace('.', '')
     centavos = produto.find('span', attrs={'class': 'price-tag-cents'})
-
-    #print(produto.prettify())
-    print('Titulo do Produto: ', titulo.text)
-    print('Link do Produto: ', link['href'])
     if(centavos):
-        print('Preço: R$', real.text + ',' + centavos.text)
+        preco = real + '.' + centavos.text
     else:
-        print('Preço: R$', real.text + ',00')
-    print('\n\n')
+        preco = real
+    row = pd.DataFrame(
+        data=[{
+        'produto': titulo.text, 
+        'link':  link['href'],
+        'preco': preco
+        }], columns=['produto', 'link', 'preco'])
+    df = pd.concat([df, row], ignore_index=True)
+filename = produto_nome + '.csv'
+df.to_csv(filename, sep=';')
